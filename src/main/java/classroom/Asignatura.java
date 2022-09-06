9833745f1813cc9d23e954598fce525f4baaaaa9
Alejandra Uribe Sierra <aluribes@unal.edu.co>
2022-09-06 10:47:37 -0500

    Update Asignatura.java
    Modificación clase Asignatura

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -4,17 +4,12 @@ public class Asignatura {
 
     String nombre;
     String n = nombre;
-    int codigoInterno;
+    int codigoInterno = 0;
     int codigoExterno;
     Tipo tipo;
 
     Asignatura() {
         this("Sin nombre",Tipo.FUNDAMENTACION);
-        this(0);
-    }
-
-    Asignatura(int codigoInterno,Tipo tipo) {
-        this("Sin nombre", codigoInterno, 0,tipo);
     }
 
     Asignatura(int codigoExterno,Tipo tipo) {
@@ -39,10 +34,6 @@ public class Asignatura {
         this.tipo = tipo;
     }
 
-    void cambiarDatos(int codigoInterno) {
-        this.codigoInterno = codigoInterno;
-    }
-
     void cambiarDatos(int codigoExterno) {
         this.codigoExterno = codigoExterno;
     }
@@ -54,7 +45,7 @@ public class Asignatura {
     public void setCodigoInterno(int codigoInterno) {
         this.codigoInterno = codigoInterno;
     }
-    
+
     public void setCodigoInterno(double codigoInterno) {
         this.codigoInterno = (int) codigoInterno;
     }
